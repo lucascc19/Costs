68704401592bfd89cdee06ea54546bf115513f7c
Lucas Rodrigues Oliveira <Lucas.Oliveira@fitbank.com.br>
2022-11-29 17:20:11 -0300

    uso dos hooks useState e useEffect

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,11 +1,16 @@
 import styles from './Select.module.css'
 
-function Select({ type, name, options, handleOnChange, value }) {
+function Select({ type, name, text, options, handleOnChange, value }) {
   return (
     <div className={styles.form_control}>
-      <label htmlFor={name}></label>
+      <label htmlFor={name}>{text}:</label>
       <select name={name} id={name}>
-        <options>Seleciona uma opção</options>
+        <option>Seleciona uma opção</option>
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
       </select>
     </div>
   )
